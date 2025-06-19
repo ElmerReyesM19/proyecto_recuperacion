2690486ad50a80d0ca3340f9a2f49096e352b94f
elmer reyes <ereyesm16@miumg.edu.gt>
2025-06-19 01:56:45 -0600

    creacion ruta para totalProductos

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const db = require('./config/db');
 const app = express()
 const {addProducto} = require('./rutas/addProducto')
+const {getTotalProductos} = require('./rutas/getTotalProductos')
 require('dotenv').config();
 app.use(express.json())
 app.post('/addProducto',addProducto)
+app.get('/totalProductos',getTotalProductos)
 
 app.listen(process.env.PORT, ()=>{
     console.log('puerto ' + process.env.PORT)
